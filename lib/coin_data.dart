7dd37de9cc77feb674ee08b4875cc25b0feebbc5
emmalexander <32887883+emmalexander@users.noreply.github.com>
2023-02-12 13:57:42 +0100

    Initial commit

---
+++ lib/coin_data.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+  'BNB',
+];
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'ECC08E9A-4096-46B9-87FF-C003BF3D1ECB';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apiKey=$apiKey';
+      http.Response response = await http.get(requestURL);
+
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
+    }
+    return cryptoPrices;
+    //TODO 4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
+    //TODO 5: Return a Map of the results instead of a single value.
+  }
+}
---
+++ lib/price_screen.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'coin_data.dart';
+import 'dart:io' show Platform;
+import 'crypto_card.dart';
+
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'AUD';
+
+  DropdownButton<String> androidDropdown() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropdownItems.add(newItem);
+    }
+
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      items: dropdownItems,
+      onChanged: (value) {
+        setState(() {
+          selectedCurrency = value;
+          getData();
+        });
+      },
+    );
+  }
+
+  CupertinoPicker iOSPicker() {
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      pickerItems.add(Text(currency));
+    }
+
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        setState(() {
+          selectedCurrency = currenciesList[selectedIndex];
+          getData();
+        });
+      },
+      children: pickerItems,
+    );
+  }
+
+  Map<String, String> coinValues = {};
+
+  bool isWaiting = false;
+  // String value = '?';
+
+  //TODO 7: Figure out a way of displaying a '?' on screen while we're waiting for the price data to come back. Hint: You'll need a ternary operator.
+
+  //TODO 6: Update this method to receive a Map containing the crypto:price key value pairs. Then use that map to update the CryptoCards.
+  void getData() async {
+    isWaiting = true;
+    try {
+      var data = await CoinData().getCoinData(selectedCurrency);
+      isWaiting = false;
+      setState(() {
+        coinValues = data;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  //TODO: For bonus points, create a method that loops through the cryptoList and generates a CryptoCard for each.
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue,
+        title: Text('🤑 Coin Ticker'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          //TODO 1: Refactor this Padding Widget into a separate Stateless Widget called CryptoCard, so we can create 3 of them, one for each cryptocurrency.
+          //TODO 2: You'll need to able to pass the selectedCurrency, value and cryptoCurrency to the constructor of this CryptoCard Widget.
+          //TODO 3: You'll need to use a Column Widget to contain the three CryptoCards.
+          Column(
+            children: [
+              CryptoCard(
+                  value: isWaiting ? '?' : coinValues['BTC'],
+                  selectedCurrency: selectedCurrency,
+                  cryptoCurrency: 'BTC'),
+              CryptoCard(
+                  value: isWaiting ? '?' : coinValues['ETH'],
+                  selectedCurrency: selectedCurrency,
+                  cryptoCurrency: 'ETH'),
+              CryptoCard(
+                  value: isWaiting ? '?' : coinValues['LTC'],
+                  selectedCurrency: selectedCurrency,
+                  cryptoCurrency: 'LTC'),
+              CryptoCard(
+                  value: isWaiting ? '?' : coinValues['BNB'],
+                  selectedCurrency: selectedCurrency,
+                  cryptoCurrency: 'BNB'),
+            ],
+          ),
+
+          Container(
+            height: 100.0,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                color: Colors.lightBlue,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20))),
+            child: Center(
+              child: Platform.isIOS ? iOSPicker() : androidDropdown(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
